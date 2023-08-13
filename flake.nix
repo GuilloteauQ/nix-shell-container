e9a58027901b05a21adddce5658757242eac0cd7
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-08-13 20:02:27 +0200

    first step in shell

--- flake.nix
+++ flake.nix
@@ -9,10 +9,31 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in
+      myShell = { buildInputs ? [], shellHook ? ""}:
+      let
+        gotainer = self.outputs.packages.${system}.gotainer;
+        underlyingShell = pkgs.mkShell {
+          buildInputs = buildInputs ++ [ pkgs.bashInteractive  ];
+          inherit shellHook;
+        };
+      in
+        pkgs.mkShell {
+          buildInputs = [ gotainer ];
+          shellHook = ''
+          ${gotainer}/bin/gotainer run  ${underlyingShell} ${pkgs.bashInteractive}
+          exit
+          '';
+        }
+     ;
+      in
     {
 
       packages.${system} = {
+        hello = myShell {
+          buildInputs = [
+            pkgs.snakemake
+          ];
+        };
         gotainer = pkgs.buildGoModule {
             name = "gotainer";
             version = "0.0";
